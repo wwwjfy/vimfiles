3e9b838e0e5f9983848eb8dea0da7d2b86ff371f
Tony Wang <tony@initialcommit.net>
2023-04-23 22:12:46 +0800

    update

--- lua/lazy_plugins.lua
+++ lua/lazy_plugins.lua
@@ -184,6 +184,7 @@ return {
         "sindrets/diffview.nvim",
         config = function()
             require("diffview").setup({
+                use_icons = false,
                 view = {
                     merge_tool = {
                         layout = "diff4_mixed",
