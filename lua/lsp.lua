188bc793b3136d5ebb093fe60cd3ced3c485ab72
Tony Wang <tony@initialcommit.net>
2026-03-09 22:54:20 +0800

    update

---
+++ lua/lsp.lua
@@ -0,0 +1,107 @@
+local function show_items(options)
+  if #options.items == 1 then
+    local tagname = vim.fn.expand('<cword>')
+    local from = vim.fn.getpos('.')
+    local current_win = vim.api.nvim_get_current_win()
+
+    local item = options.items[1]
+    local b = item.bufnr or vim.fn.bufadd(item.filename)
+
+    vim.cmd("normal! m'")
+    local tagstack = { { tagname = tagname, from = from } }
+    vim.fn.settagstack(vim.fn.win_getid(current_win), { items = tagstack }, 't')
+
+    vim.bo[b].buflisted = true
+
+    -- open in current window if it matches
+    local wins = vim.fn.win_findbuf(b)
+    local w = wins[1] or 0
+    for _, v in ipairs(wins) do
+      if v == current_win then
+        w = v
+        break
+      end
+    end
+
+    vim.api.nvim_win_set_buf(w, b)
+    vim.api.nvim_win_set_cursor(w, { item.lnum, item.col - 1 })
+    vim._with({ win = w }, function()
+      vim.cmd('normal! zv')
+    end)
+
+    return
+  end
+
+  vim.fn.setqflist({}, " ", {
+    title = options.title,
+    items = options.items,
+  })
+  vim.cmd('botright copen')
+end
+
+local goto_implementations = function()
+  vim.lsp.buf.implementation({ on_list = function(options)
+    local ft = vim.api.nvim_get_option_value("filetype", {})
+
+    if ft == "go" and options.items ~= nil then
+      local items = vim.tbl_filter(function(v)
+        return not string.find(v.filename, "mock_") and not string.find(v.filename, "mocks/")
+      end, options.items)
+
+      if #items > 0 then
+        options.items = items
+      end
+    end
+
+    show_items(options)
+  end})
+end
+
+local function show_references_no_tests()
+  vim.lsp.buf.references({ includeDeclaration = false }, { on_list = function(options)
+    local ft = vim.api.nvim_get_option_value("filetype", {})
+
+    if ft == "go" and options.items ~= nil then
+      local items = vim.tbl_filter(function(v)
+        return not string.find(v.filename, "_test.go")
+      end, options.items)
+
+      if #items > 0 then
+       options.items = items
+      end
+    end
+
+    show_items(options)
+  end})
+end
+
+local on_attach = function(_, bufnr)
+  local opts = { noremap=true, silent=true, buffer = bufnr }
+
+  vim.keymap.set("n", "gdd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "gdt", function()
+      vim.cmd("tab split")
+      vim.lsp.buf.definition()
+  end, opts)
+  vim.keymap.set("n", "gds", function()
+      vim.cmd("vsplit")
+      vim.lsp.buf.definition()
+  end, opts)
+  vim.keymap.set("n", "gri", function()
+      vim.cmd("tab split")
+      goto_implementations()
+  end, opts)
+  vim.keymap.set("n", "grr", show_references_no_tests, opts)
+  vim.keymap.set("n", "cn", function() vim.diagnostic.jump({ count = 1 }) end, opts)
+
+  require("lsp_signature").on_attach({
+      bind = true, -- This is mandatory, otherwise border config won"t get registered.
+      handler_opts = {
+        border = "rounded"
+      }
+  })
+end
+
+return {
+  on_attach = on_attach,
+}
